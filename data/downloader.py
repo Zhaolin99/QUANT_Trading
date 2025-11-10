f20f5ba999fd32b44750ed89c1065060337efe3c
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-11-10 13:34:47 +0800

    Update downloader.py

--- data/downloader.py
+++ data/downloader.py
@@ -1,1 +1,9 @@
+import yfinance as yf
+from zoneinfo import ZoneInfo
+
+def download_ohlcv(symbol, period="730d", interval="60m"):
+    df = yf.download(symbol, period=period, interval=interval, auto_adjust=True, progress=False)
+    if df.index.tz is None:
+        df = df.tz_localize("UTC")
+    return df.tz_convert(ZoneInfo("Asia/Hong_Kong"))[["Open","High","Low","Close","Volume"]].dropna()
 
